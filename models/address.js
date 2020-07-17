19eb2d6a231116323c1bdf21a93c78672f85bd22
Dmytro Didukh <jeriko.outback@gmail.com>
2020-07-17 21:13:39 +0300

    deleted trash

--- models/address.js
+++
@@ -1,9 +0,0 @@
-import {Schema, model} from 'mongoose';
-
-const addressSchema = new Schema({
-    street: String,
-    built: String,
-    apartment: String,
-});
-
-export default model('Address', addressSchema);
